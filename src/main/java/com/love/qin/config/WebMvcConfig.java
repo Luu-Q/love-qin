41c188de0738ecb841f0b7a83f0ce6e553182411
luning <luning@yktour.com.cn>
2019-04-18 18:02:57 +0800

    修改日期

--- src/main/java/com/love/qin/config/WebMvcConfig.java
+++ src/main/java/com/love/qin/config/WebMvcConfig.java
@@ -1,9 +1,7 @@
 package com.love.qin.config;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootConfiguration
@@ -12,16 +10,10 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-
-        registry.addResourceHandler("/webjars/**","/static/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/","classpath:/META-INF/resources/static/");
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
         super.addResourceHandlers(registry);
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-        super.addViewControllers(registry);
-    }
 }
---
+++ src/main/java/com/love/qin/controller/HtmlController.java
@@ -0,0 +1,15 @@
+package com.love.qin.controller;
+
+/**
+ * @description: html页面跳转
+ * @author: LUNING
+ * @create: 2019-04-18 15:37
+ */
+//@Controller
+public class HtmlController {
+
+//    @GetMapping(value = "index2")
+//    public String index2(){
+//        return "index2";
+//    }
+}
