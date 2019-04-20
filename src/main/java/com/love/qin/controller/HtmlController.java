5fc5f85f7efde75a9b83333a30bff066f130e1fa
luning <luning@yktour.com.cn>
2019-04-20 23:49:10 +0800

    ss

--- src/main/java/com/love/qin/controller/HtmlController.java
+++ src/main/java/com/love/qin/controller/HtmlController.java
@@ -1,6 +1,8 @@
 package com.love.qin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @description: html页面跳转
@@ -8,18 +10,19 @@ import org.springframework.stereotype.Controller;
  * @create: 2019-04-18 15:37
  */
 @Controller
+@RequestMapping("/")
 public class HtmlController {
-//
-//    @GetMapping("/pc")
-//    public String pc() {
-//
-//        return "index_pc";
-//    }
-//
-//    @GetMapping("/mo")
-//    public String mo() {
-//
-//        return "index_mo";
-//    }
+
+    @GetMapping("/lovel")
+    public String pc() {
+
+        return "lovel";
+    }
+
+    @GetMapping("/loveq")
+    public String mo() {
+
+        return "loveq";
+    }
 
 }
